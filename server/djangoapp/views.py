95eeeee07de348281c15b28a6ae89e932f49b680
polymathdanny <87679694+polymathdanny@users.noreply.github.com>
2022-05-01 15:00:00 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -66,7 +66,7 @@ def registrationPage(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     if request.method == "GET":
-        url = 'https://djangoapp1.us-south.cf.appdomain.cloud/api/dealerships/'
+        url = 'https://98ff71f8-4e54-4bcc-bfbd-e91475399743-bluemix.cloudantnosqldb.appdomain.cloud/api/dealerships/'
 
         # Get dealers from the URL
         dealerships = get_dealers_from_dict(url)
@@ -98,7 +98,7 @@ def get_dealerships(request):
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships_by_id(request, pk, val):
     if request.method == "GET":
-        url = 'https://djangoapp1.us-south.cf.appdomain.cloud/api/dealerships/' + pk +'/' + val + '/'
+        url = 'https://98ff71f8-4e54-4bcc-bfbd-e91475399743-bluemix.cloudantnosqldb.appdomain.cloud/api/dealerships/' + pk +'/' + val + '/'
         print(url)
         # Get dealers from the URL
         dealerships = get_dealers_from_cf(url)
@@ -109,7 +109,7 @@ def get_dealerships_by_id(request, pk, val):
 
 def get_dealerships_by_state(request, val):
     if request.method == "GET":
-        url = 'https://djangoapp1.us-south.cf.appdomain.cloud/api/dealerships/state/' + val + '/'
+        url = 'https://98ff71f8-4e54-4bcc-bfbd-e91475399743-bluemix.cloudantnosqldb.appdomain.cloud/api/dealerships/state/' + val + '/'
         # Get dealers from the URL
         dealerships = get_dealers_from_dict(url)
         states = get_dealerships_by_states_from_dict(url)
@@ -201,7 +201,7 @@ def add_review(request):
 
         review_id = str(request.GET.get('dealership'))
         print(review_id)
-        url = 'https://djangoapp1.us-south.cf.appdomain.cloud/api/reviews/dealership/' + review_id 
+        url = 'https://98ff71f8-4e54-4bcc-bfbd-e91475399743-bluemix.cloudantnosqldb.appdomain.cloud/api/reviews/dealership/' + review_id 
         print(url)
         reviews = get_reviews_from_dict(url)
         form = CreateReviewForm()
